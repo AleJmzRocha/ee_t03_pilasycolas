20a291e6391a03199ed9b27f257c81bc50501624
Usuario <Usuario@172.16.189.106>
2016-09-19 09:45:06 -0500

    commit inicial

--- src/ee_t03_pilasycolas/Test.java
+++ src/ee_t03_pilasycolas/Test.java
@@ -2,34 +2,38 @@ package ee_t03_pilasycolas;
 import java.io.*;
 
 public class Test {
+	/**
+	 *Clase que implementa pilas y colas 
+	 */
 	Pila<String> pila = new Pila<String>();
 	Cola<String> cola = new Cola<String>();
 	
-	public void leer(){
+	public void leer(){		
+		//Lee el archivo
 		File file = new File("datos.txt");
 		try{
 			BufferedReader in = new BufferedReader(new FileReader(file));
 			String s;
-			s = in.readLine();
-			if(s.equals("0")){
+			s = in.readLine(); //Lee la pimera línea del documento
+			if(s.equals("0")){ //Si el primer número es 0, usará pilas
 				System.out.println("Pila:");
-				s = in.readLine();
+				s = in.readLine(); //Lee la siguiente línea
 				while(s != null){
-					if(s.substring(0,1).equals("0")){
+					//Si se antepone un 0, se meterá a la pila
+					if(s.substring(0,1).equals("0"))
 						pila.push(s.substring(2,3));
-					}
 					else{
-						if(s.equals("1"))
+						if(s.equals("1")) //Si hay un uno, saca de la pila un elemento
 							System.out.println(pila.pop());
 						else
-							if(s.equals("2"))
+							if(s.equals("2")) //Si es un dos, muestra lo que tiene la pila en el tope
 								System.out.println(pila.peek());
 							else
 								System.out.println("No existe la instrucción.");
 					}
-					s = in.readLine();
+					s = in.readLine(); //Lee la siguiente línea y regresa al ciclo
 				}		
-				in.close();
+				in.close(); //Se cierra la lectura del documento
 			}
 		}catch (FileNotFoundException e1) {
 		      System.err.println("El archivo: " + file + " no existe");
@@ -39,23 +43,26 @@ public class Test {
 	}
 	
 	public void leer2(){
+		//Lee el archivo
 		File file = new File("datos2.txt");
 		try{
 			BufferedReader in = new BufferedReader(new FileReader(file));
 			String s;
-			s = in.readLine();
-			if(s.equals("1")){
+			s = in.readLine(); //Lee la pimera línea del documento
+			if(s.equals("1")){ //Si el primer número es 1, usará colas
 				System.out.println("Cola:");
-				s = in.readLine();
+				s = in.readLine(); //Lee la siguiente línea
 				while(s != null){
-					if(s.substring(0,1).equals("0")){
+					if(s.substring(0,1).equals("0")) 
+						//Si se antepone un cero, se insertará en la cola el elemento siguiente
 						cola.insertar(s.substring(2,3));
-					}else
+					else
 						if(s.equals("1"))
+							//Si es un uno, se sacará de la cola un elemento
 							System.out.println(cola.retirar());
-					s = in.readLine();
+					s = in.readLine(); //Lee la siguiente línea y regresa al ciclo
 				}
-				in.close();
+				in.close(); //Se cierra la lectura del documento
 			}
 		}catch (FileNotFoundException e1) {
 		      System.err.println("El archivo: " + file + " no existe");
@@ -65,7 +72,9 @@ public class Test {
 	}
 	
 	public static void main(String[] args){
+		//Se instancia un objeto de la clase
 		Test t = new Test();
+		//Se prueban ambos métodos
 		t.leer();
 		System.out.println();
 		t.leer2();
